d01d084d0a17e944e46893cbb00efa1e99692985
fxzer <fxzer8888@163.com>
2024-05-30 20:14:48 +0800

    fix: style

--- components/global/RadioGroup.vue
+++ components/global/RadioGroup.vue
@@ -7,7 +7,7 @@ const value = defineModel<string>()
   <div class="flex-center overflow-hidden border-1 rounded-1 bg-white text-gray-600 divide-x-1 dark:border-none">
     <div
       v-for="item in list" :key="item.value" class="h-full text-gray-600"
-      :class="value === item.value ? 'bg-primary text-white' : '' "
+      :class="value === item.value ? 'bg-primary !text-white' : '' "
     >
       <input :id="item.value" v-model="value" type="radio" :value="item.value" class="hidden">
       <label :for="item.value" class="h-full flex-center cursor-pointer px-4 py-1">{{ item.label }}</label>
